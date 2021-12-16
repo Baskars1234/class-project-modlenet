998778eababc4664a92c4c1a8d2e50bfc71f06c4
Baskars1234 <76224896+Baskars1234@users.noreply.github.com>
2021-12-16 19:32:15 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,7 +3,34 @@ function setup() {
   canvas.center();
   video = createCapture(VIDEO);
   video.hide();
+  classifier = ml5.imageClassifier('MobileNet',modelLoaded);
 }
 
+function modelLoaded(){
+  console.log('model Loaded!');
+}
 
+function draw() {
+  image(video, 0, 0, 300, 300);
+  classifier.classify(video, gotResult);
+}
+
+var previous_result = '';
+
+function gotResult(error, results) {
+  if(error) {
+    console.log(error);
+  } else {
+    if((results[0].confidence > 0.5) && (previous_result != results[0].label)){
+    console.log(results);
+    previous_results = results[0].label;
+    var synth = window.speechSynthesis;
+    var utterThis = new SpeechSynethesisUtterance(speak_data);
+    synth.speak(utterThis);
+
+    document.getElementbyId("result_object_name").innerHTML = results[0].label;
+    document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+  }
+ }
+}
 
